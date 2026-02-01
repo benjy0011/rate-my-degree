5cd48811c5443269ab8d6e839767ed7058e58bc7
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2026-02-01 12:49:55 +0800

    feat: join the conversation section

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -4,6 +4,7 @@ import Hero from "@/components/HomePage/Hero/Hero"
 import RecentReviews from "@/components/HomePage/RecentReviews/RecentReviews"
 import SellingPoints from "@/components/HomePage/Intro/SellingPoints"
 import TopRatedDegree from "@/components/HomePage/TopRatedDegree/TopRatedDegree"
+import JoinTheConversation from "@/components/HomePage/JoinTheConversation/JoinTheConversation"
 
 export default function Home() {
   return (
@@ -17,6 +18,8 @@ export default function Home() {
       <TopRatedDegree />
 
       <RecentReviews />
+
+      <JoinTheConversation />
     </>
   )
 }
--- src/components/HomePage/JoinTheConversation/JoinTheConversation.tsx
+++ src/components/HomePage/JoinTheConversation/JoinTheConversation.tsx
@@ -1,6 +1,28 @@
+import SignInButton from "@/components/Navbar/SignInButton"
+import RedirectButton from "@/components/RedirectButton"
+
 const JoinTheConversation = () => {
   return (
-    <div>JoinTheConversation</div>
+    <section id="join-the-conversation" className="join-the-conversation">
+      <div className="join-the-conversation-wrapper">
+        <h3>Join the Conversation</h3>
+
+        <p>Help future students make better decisions. Share your university experience today.</p>
+
+        <div className="flex gap-4 max-sm:flex-col max-sm:gap-6">
+          <RedirectButton
+            text="Write a Review"
+            path="reviews"
+            className="bg-yellow-300 h-full px-6 py-3 text-black"
+          />
+          <SignInButton
+            text="Create Account"
+            className="border-gray-300 h-full px-6 py-3"
+            wrapperClassName="bg-gray-700"
+          />
+        </div>
+      </div>
+    </section>
   )
 }
 export default JoinTheConversation
