845505b2e6978f5b483d97a3e0a4db43ce2d5741
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2026-02-03 23:06:11 +0800

    feat(utils): date formater

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function dateFormat(
+  date: string | Date,
+) {
+  const parsed = new Date(date);
+
+  const formatted = new Intl.DateTimeFormat("en-GB", {
+    month: "short",
+    year: "numeric",
+  }).format(parsed);
+
+  return formatted
+}
